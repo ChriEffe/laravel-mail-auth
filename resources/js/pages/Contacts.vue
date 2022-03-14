43f4b8200d98976e8e3be7ef38032847fc3dfe2f
ChriEffe <christianferrazzano93@gmail.com>
2022-03-14 16:12:38 +0100

    add vue router

--- resources/js/pages/Contacts.vue
+++ resources/js/pages/Contacts.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-        Contact
+        Contacts
     </div>
 </template>
 
 <script>
 export default {
-    name: 'contact'
+    name: 'contacts'
 }
 </script>
 
---
+++ resources/js/views/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Header/>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue';
+import Main from '../components/Main.vue';
+
+export default {
+    name: 'app',
+    components: {
+        Header,
+        Main
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
